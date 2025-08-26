d1c628e497336296030cfd8082b3bc46667e2999
jns-w <jns.w@icloud.com>
2025-08-26 17:30:39 +0800

    fix: typo & inspections

---
+++ app/index.tsx
@@ -0,0 +1,41 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { useTheme } from "@/context/ThemeContext";
+import { COLORS, type Theme, TYPOGRAPHY } from "@/styles/Constants";
+
+export default function Index() {
+  const { theme, toggleTheme } = useTheme();
+
+  const styles = createThemedStyles(theme);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.introText}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+      <Pressable onPress={() => toggleTheme()}>
+        <Text style={styles.swapBtn}>
+          {theme.name === "light" ? "go dark" : "go light"}
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function createThemedStyles(theme: Theme) {
+  return StyleSheet.create({
+    container: {
+      alignItems: "center",
+      backgroundColor: theme.bg1,
+      flex: 1,
+      justifyContent: "center",
+    },
+    introText: {
+      ...TYPOGRAPHY.bodyL,
+      color: theme.text,
+    },
+    swapBtn: {
+      color: theme.name === "light" ? COLORS.sky[300] : COLORS.primary[200],
+    },
+  });
+}
